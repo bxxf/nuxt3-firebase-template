b68a52378bd2a527d8b78796ab0452d224d3d440
Filip Brebera <breberaf@gmail.com>
2022-05-01 22:08:02 +0200

    try

---
+++ public/sw.js
@@ -0,0 +1,76 @@
+importScripts(
+  "https://www.gstatic.com/firebasejs/9.7.0/firebase-app-compat.js"
+);
+importScripts(
+  "https://www.gstatic.com/firebasejs/9.7.0/firebase-auth-compat.js"
+);
+const app = firebase.initializeApp({
+  apiKey: "AIzaSyCeU5j8VhI4djofAj7DGXeojqYcbytc5l0",
+  authDomain: "fire-nuxt-template.firebaseapp.com",
+});
+
+const authService = firebase.auth();
+
+const getIdToken = () => {
+  return new Promise((resolve) => {
+    const unsubscribe = authService.onAuthStateChanged((user) => {
+      unsubscribe();
+      if (user) {
+        // force token refresh as it might be used to sign in server side
+        user.getIdToken(true).then(
+          (idToken) => {
+            resolve(idToken);
+          },
+          () => {
+            resolve(null);
+          }
+        );
+      } else {
+        resolve(null);
+      }
+    });
+  });
+};
+
+const fetchWithAuthorization = async (original, idToken) => {
+  // Clone headers as request headers are immutable.
+  const headers = new Headers();
+  for (let entry of original.headers.entries()) {
+    headers.append(entry[0], entry[1]);
+  }
+
+  // Add ID token to header.
+  headers.append("Authorization", "Bearer " + idToken);
+
+  // Create authorized request
+  const { url, ...props } = original.clone();
+  const authorized = new Request(url, {
+    ...props,
+    mode: "same-origin",
+    redirect: "manual",
+    headers,
+  });
+
+  return fetch(authorized);
+};
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    getIdToken().then((idToken) =>
+      idToken
+        ? // if the token was retrieved we attempt an authorized fetch
+          // if anything goes wrong we fall back to the original request
+          fetchWithAuthorization(event.request, idToken).catch(() =>
+            fetch(event.request)
+          )
+        : // otherwise we return a fetch of the original request directly
+          fetch(event.request)
+    )
+  );
+
+  return;
+});
+
+self.addEventListener("activate", (ev) => {
+  ev.waitUntil(self.clients.claim());
+});
